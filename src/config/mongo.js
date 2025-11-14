70652dfab4cc7ead0456ed7e07404ebccee115f8
haisyaf <haidar.syaafie@gmail.com>
2025-11-14 18:00:30 +0700

    fix: remove deprecated useNewUrlParser option from MongoDB connection

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
     console.log('Database Name:', mongoose.connection.name);
   } catch (error) {
