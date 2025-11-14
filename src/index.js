f688299f77f54814cc24e7ef2e2609954102d488
Haidar Syaafi <146041451+haisyaf@users.noreply.github.com>
2025-11-14 11:38:50 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ app.use('/api/reviews', reviewRoutes);
 app.use('/api/notifications', notificationRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
